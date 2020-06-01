006523c114dd3a0bc8d81f45affedbbdb75bf0c2
Konan Maruyama <zepp25458565@gmail.com>
2020-06-01 12:53:06 +0700

    Made new view and set new page as root.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root to: 'posts#new'
   resources :posts, only:[:new, :create, :update, :edit, :show]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
