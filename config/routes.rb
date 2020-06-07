102401ceb05271eb76e9b38d232d5bfbbb731722
Konan Maruyama <zepp25458565@gmail.com>
2020-06-08 01:01:22 +0700

    deleted before action

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   root to: 'posts#new'
   get 'posts/confirm/:id', to: 'posts#confirm', as: 'confirm'
   get 'posts/download/:id', to: 'posts#download', as: 'download'
-  resources :posts, only:[:new, :create, :update, :edit, :show]
+  resources :posts, only:[:new, :create]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
