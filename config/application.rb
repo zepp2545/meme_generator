d4fd0d439c2c46b6d6c9b11a1aeae0d14d173322
Konan Maruyama <zepp25458565@gmail.com>
2020-06-08 01:51:33 +0700

    Modified autoloading in production.

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,7 @@ module Myapp
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
+    config.enable_dependency_loading = true
     config.autoload_paths << Rails.root.join("lib")
 
     # Settings in config/environments/* take precedence over those specified here.
