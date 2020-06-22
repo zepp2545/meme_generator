6e3016d0f244c4e0435277ae492f81b9bd6a7910
Konan Maruyama <zepp25458565@gmail.com>
2020-06-23 01:56:07 +0700

    Deleted some files

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,13 +1,9 @@
 class PostsController < ApplicationController
 
-  
-  # GET /posts/new
   def new
     @post = Post.new
   end
 
-  # POST /posts
-  # POST /posts.json
   def create
     @post = Post.new(post_params)
     @post.image = Meme.new(params[:post][:image].tempfile.path, @post.text_color, @post.upper_text, @post.lower_text).store_meme
@@ -31,17 +27,14 @@ class PostsController < ApplicationController
     data = open @post.image, "r"
 
     send_data data.read, filename: @post.image.split("/").last
-
   end
 
 
   private
-    # Use callbacks to share common setup or constraints between actions.
     def set_post
       @post = Post.find(params[:id])
     end
 
-    # Only allow a list of trusted parameters through.
     def post_params
       params.require(:post).permit(:image, :text_color, :upper_text, :lower_text)
     end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,4 @@ Rails.application.routes.draw do
   get 'posts/confirm/:id', to: 'posts#confirm', as: 'confirm'
   get 'posts/download/:id', to: 'posts#download', as: 'download'
   resources :posts, only:[:new, :create]
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
